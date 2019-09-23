263df405a6ae4bf483d954082257795bc728f8cb
andreas-sot <33868513+andreas-sot@users.noreply.github.com>
2019-09-24 00:50:35 +0300

    Finding comigs using RL

--- everything_else/codes/test/finding best quad using RL.py
+++ everything_else/codes/test/finding best quad using RL.py
@@ -1,42 +1,31 @@
 import numpy as np
-from scipy.io import loadmat
+from scipy.io import savemat # ,loadmat
+import matlab.engine
+from sklearn.neural_network import MLPClassifier
 
 # hyperparameters
-n = 7              # number of variables
-aux = True
-conflict_threshold = 0.1
+n = 7              # number of variables including aux
+aux = 1
+num_cases = 3
+new_input = True
+
 n_sessions = 100
-t_max = 50
+t_max = 150
+
 c_conflict = 1
 c_distance = 1
-c_entropy = 0.4
+c_entropy  = 0.25
+
+c_num_of_terms = 1
+c_strength = 2
+c_accuracy = 10000
+
 percentile = 70
-hidden_layers = (20,30,40)
-np.random.seed(1)
+max_comigs = 10
+comigs_each_run = 1
 # -----------------------------------------------------------------------------------------
 
-coeffs_size = int( n*(n+1)/2 )
-n_actions = 2*coeffs_size + 1     # number of actions
-
-if aux:
-    LHS_size = 2**(n-1)
-else:
-    LHS_size = 2**n
-
-LHS = loadmat('data.mat')['LHS']
-LHS.shape = (LHS_size,1)
-
-allbits = loadmat('data.mat')['allbits']
-allbits = np.transpose(allbits)
-
-reset_state = loadmat('data.mat')['reset_state']
-reset_state.shape = (coeffs_size,-1)
-reset_state = np.repeat(reset_state,n_sessions,1)
-
-one_hot = np.zeros((coeffs_size,n_actions))
-for i in range(coeffs_size):
-    one_hot[i,i] = 1
-    one_hot[i,coeffs_size+i] = -1
+# functions/classes
 
 class environment:
     
@@ -52,61 +41,45 @@ def reward(conflict, dist, entropy):
     r_conflict = 1 - conflict**(1/3)             # "preserving states" reward
     r_distance = 1 - dist**(1/3)                 # "staying close"     reward
     r_entropy  = entropy                         # "being an explorer" reward
-    #if np.max(np.abs(new_s)) >= 4:
-    #    r -= 1                           # punishment for large coefficients
     
     return c_conflict * r_conflict + c_distance * r_distance + c_entropy * r_entropy
 
 def rhs(s):
     RHS = allbits.dot(s)
-    RHS = RHS - np.min(RHS,0)
-    if aux:
-        RHS = np.minimum(RHS[::2],RHS[1::2])    # when using aux
+    if aux == 1:
+        RHS  = np.minimum(RHS[::2],RHS[1::2])
+    if aux == 2:
+        min1 = np.minimum(RHS[::4],RHS[1::4])
+        min2 = np.minimum(RHS[2::4],RHS[3::4])
+        RHS  = np.minimum(min1,min2)
     
     return RHS
 
-
 def accuracy(s):
-    RHS = rhs(s)
-    difference = np.abs( np.subtract(RHS,LHS) )
-    distance = np.sum(difference,0)
-    distance = np.minimum(1,distance/12) #LHS_size? or maybe changing at runtime?
-    conflict = np.sum(difference != 0,0) / LHS_size
     
-    return conflict, distance
-
-
-# create agent
-from sklearn.neural_network import MLPClassifier
-agent = MLPClassifier(
-    hidden_layer_sizes = hidden_layers,
-    activation='tanh',
-    warm_start=False,#True,  # keep progress between .fit() calls
-    max_iter=1,       # make only 1 iteration on each .fit()
-
-)
-env = environment()
-# initialize agent to the dimension of state and number of actions
-init_training = loadmat('data.mat')['input']
-size = init_training.shape[0]
-
-train_data = np.subtract(init_training,one_hot[:,0])
-target = np.ones((size,1))*0
-for action in range(1,n_actions):
-    train_data = np.append( train_data, np.subtract(init_training,one_hot[:,action]) )
-    target = np.append( target, np.ones((size,1))*action )
-
-train_data = train_data.reshape(-1,coeffs_size)
-
-agent.fit(train_data, target)
-
+    RHS = rhs(s)
+    difference = np.subtract(RHS,LHS)
+    flag = (np.sum(difference < 0,0) != 0)
+    
+    conflict = np.sum(difference[mask] != 0,0) / sum(mask)
+    
+    difference = difference[difference < 0]
+    difference = np.abs(difference)
+    distance = np.multiply( np.sum(difference,0) ,flag.reshape(-1,) )
+    distance = np.minimum(1, distance/sum(mask)) #LHS_size? or maybe changing at runtime?
+    
+    return conflict, distance, flag
 
 def generate_session(t_max = 50):
+    global count, worst_score, worst_index, stop_condition, good, found
     states,actions = [],[]
     total_reward = 0
-   
+    mini = 100
+    #r_best = -1000 * np.ones((n_sessions,))
     s = env.reset()
-        
+    #conflict,dist = accuracy(s)
+    #old_reward = reward(conflict, dist, 0)
+    
     for t in range(t_max):
         
         probs = agent.predict_proba(np.transpose(s))
@@ -118,30 +91,75 @@ def generate_session(t_max = 50):
         
         new_s = env.step(a)
         
-        conflict,dist = accuracy(new_s)
+        conflict, dist, flag = accuracy(new_s)
+        
         entropy = -np.sum( np.multiply(probs,np.log(probs)/np.log(coeffs_size)), 1)
         
-        r = reward(conflict, dist, entropy)
+        #new_reward = reward(conflict, dist, entropy)
+        #r = new_reward - old_reward
+        #old_reward = new_reward
         
-        if any(conflict <= conflict_threshold):
-            #flag = True
-            #for k in range(len(good)):
-            #    if (good[k] == new_s).all:
-            #        flag = False
-            #if flag:
-            #   good.append(new_s)
-            #print(conflict)
-            print( new_s[:,conflict <= conflict_threshold], 100*(1-conflict[conflict <= conflict_threshold]) )
-            wait = input("press enter to continue")
+        r = reward(conflict, dist, entropy)
+        r[flag] -= 10
+        
+        condition = ( flag == 0 )
+        
+        if any(condition):
+            coeffs_strength = np.sum(np.abs(new_s[:,condition]),0)
+            coeffs_number = np.sum( new_s[:,condition] != 0 , 0 )
+            score = c_accuracy*(1 - conflict[condition]) - c_strength*coeffs_strength - c_num_of_terms*coeffs_number
+            score_condition = ( score > worst_score )
+        
+            if any(score_condition):
+                stop_condition = 0
+                found = 1
+                #flag = True
+                #for k in range(len(good)):
+                #    if (good[k] == new_s[:,temp]).all():
+                #        flag = False
+                #if flag:
+                #   good.append(new_s[:,temp])
+                #print(rhs(new_s[:,temp]))
+                coeffs = np.transpose(new_s[:,condition])
+                coeffs = coeffs[score_condition]
+                score = score[score_condition]
+                #print(j,max(score),worst_score)
+                for i in range(coeffs.shape[0]):
+                    if score[i] > worst_score:
+                        if count < comigs_each_run:
+                            good[num_COMIGs,count] = coeffs[i]
+                            count = count + 1
+                        else:
+                            good[num_COMIGs,worst_index] = coeffs[i]
+                        
+                        if count == comigs_each_run:
+                            worst_score = 10000
+                            worst_index = 10000
+                            for it in range(comigs_each_run):
+                                coef = good[num_COMIGs,it]
+                                conflict_current, _, _ = accuracy(coef.reshape(-1,1))
+                                coeffs_strength = np.sum(np.abs(coef))
+                                coeffs_number = np.sum(coef != 0)
+                                score_current = c_accuracy*(1 - conflict_current) - c_strength*coeffs_strength - c_num_of_terms*coeffs_number
+                                if score_current < worst_score:
+                                    worst_score = score_current
+                                    worst_index = it
+        
+        if min(conflict) < mini:
+            mini = min(conflict)
         
         states.append(np.transpose(s))
         actions.append(a)
         total_reward += r
         
         s = new_s
-        
-    return states, actions, total_reward
-
+    
+    stop_condition += 1
+    flag = False
+    if stop_condition == 40:
+        flag = True
+    #print(1-mini)
+    return states, actions, total_reward, flag, found
 
 def select_elites(states_batch,actions_batch,rewards_batch,percentile=50):
     """
@@ -164,17 +182,15 @@ def select_elites(states_batch,actions_batch,rewards_batch,percentile=50):
     
     return elite_states, elite_actions
 
-
-from IPython.display import clear_output
-
-def show_progress(batch_rewards, percentile, reward_range=[0,100]):
+def show_progress(batch_rewards, percentile):
     
     mean_reward, threshold = np.mean(batch_rewards), np.percentile(batch_rewards, percentile)
-    Log.append([int(mean_reward), int(threshold)])
-
-    clear_output(True)
+    #print("mean reward = %.3f, threshold=%.3f"%(mean_reward, threshold))
+    #Log.append([int(mean_reward), int(threshold)])
+    
     print("mean reward = %.3f, threshold=%.3f"%(mean_reward, threshold))
-    """
+    return mean_reward
+    '''
     plt.figure(figsize=[8,4])
     plt.subplot(1,2,1)
     plt.plot(list(zip(*log))[0], label='Mean rewards')
@@ -189,24 +205,167 @@ def show_progress(batch_rewards, percentile, reward_range=[0,100]):
     plt.grid()
 
     plt.show()
-    """
+    '''
+#-----------------------------------------------------------------------------------------------
 
-Log  = []
-good = []
-for i in range(500):
-    #sessions = [ generate_session() for _ in range(n_sessions) ]
-    sessions = generate_session(t_max)
+# Run for multiple LHSs
+
+for Case in range(num_cases):
+    # Read new input
     
-    #batch_states,batch_actions,batch_rewards = map(np.array, zip(*sessions))
-    batch_states,batch_actions,batch_rewards = sessions[0], sessions[1], sessions[2]
-    #print(batch_rewards)
-    #wait = input("rewards")
-    elite_states, elite_actions = select_elites(batch_states, batch_actions, batch_rewards, percentile)
+    if new_input:
+        
+        # Run Matlab to pretrain RL model
+        eng = matlab.engine.start_matlab()
+        #eng.brute_force_for_RL_input_data(nargout = 0)
+        [init_training, LHS, allbits, reset_state] = eng.pretrain(Case, n, aux, nargout = 4)
+        
+        coeffs_size = int( n*(n+1)/2 ) + 1
+        n_actions = 2*coeffs_size + 1   # number of actions
+        
+        LHS_size = 2**(n-aux)
+        
+        #LHS = loadmat('data.mat')['LHS']
+        LHS = np.array(LHS)
+        LHS.shape = (LHS_size,1)
+        
+        #allbits = loadmat('data.mat')['allbits']
+        allbits = np.array(allbits)
+        allbits = np.transpose(allbits)
+        
+        #reset_state = loadmat('data.mat')['reset_state']
+        reset_state = np.array(reset_state)
+        reset_state.shape = (coeffs_size,-1)
+        reset_state = np.repeat(reset_state,n_sessions,1)
+        
+        one_hot = np.zeros((coeffs_size,n_actions))
+        for i in range(coeffs_size):
+            one_hot[i,i] = 1
+            one_hot[i,coeffs_size+i] = -1
     
-    agent.partial_fit(elite_states, elite_actions)
+    # create agent
     
-    show_progress(batch_rewards, percentile, reward_range=[0,np.max(batch_rewards)])
+    agent = MLPClassifier(
+        hidden_layer_sizes = (coeffs_size,2*coeffs_size),
+        activation='tanh',
+        warm_start=False,#True,  # keep progress between .fit() calls
+        max_iter=1,       # make only 1 iteration on each .fit()
+    )
     
-#    if np.mean(batch_rewards)> 50:
- #       print("You Win!!! You may stop training now")
+    env = environment()
+    
+    #init_training = loadmat('data.mat')['input']
+    init_training = np.array(init_training)
+    size = init_training.shape[0]
+    
+    train_data = np.subtract(init_training,one_hot[:,0])
+    target = np.ones((size,1))*0
+    for action in range(1,n_actions):
+        train_data = np.append( train_data, np.subtract(init_training,one_hot[:,action]) )
+        target = np.append( target, np.ones((size,1))*action )
+    
+    train_data = train_data.reshape(-1,coeffs_size)
+    
+    #------------------------------------------------------------------------------------------------
+    # Finding COMIGs
+    
+    best_num_of_comigs = 1000
+    for Run in range(3):
+        good = np.zeros((max_comigs,comigs_each_run,coeffs_size))
+        good_ind = []
+        score = 0
+        
+        for num_COMIGs in range(max_comigs):
+            
+            coef = np.zeros((max_comigs,coeffs_size))
+            for i in range(num_COMIGs):
+                coef[i] = good[i,good_ind[i]]
+            
+            mask = np.array([True]*LHS_size)
+            mask = mask.reshape(-1,1)
+            mask_preserve = np.zeros((max_comigs,LHS_size,1))
+            
+            for i in np.arange(num_COMIGs):
+                RHS = rhs(coef[i].reshape(-1,)).reshape(-1,1)
+                mask_preserve[i] = np.subtract(RHS,LHS) != 0
+                mask = np.multiply(mask, mask_preserve[i])
+            
+            mask = mask.reshape(-1,)
+            mask = mask == 1
+            if sum(mask) == 0:
+                break
+            print('states left = ', sum(mask))
+            
+            count = 0
+            worst_score = -10000
+            worst_index = -10000
+            stop_counter = 0
+            counter = 0
+            found = 0
+            while counter < 4:
+                stop_condition = 0
+                np.random.seed()
+                agent.fit(train_data,target) # initialize agent
+                for j in range(100):
+                    #mean_reward_max_in = -10000
+                    
+                    sessions = generate_session(t_max)
+                    
+                    batch_states,batch_actions,batch_rewards, flag = sessions[0], sessions[1], sessions[2], sessions[3]
+                    
+                    if flag:
+                        break
+                    
+                    elite_states, elite_actions = select_elites(batch_states, batch_actions, batch_rewards, percentile)
+                    
+                    agent.partial_fit(elite_states, elite_actions)
+                    
+                    '''
+                    flag = False
+                    if j % 10 == 0:
+                        flag = True
+                    
+                    mean_reward = show_progress(batch_rewards, percentile)#, flag)
+                    
+                    if mean_reward > mean_reward_max_in:
+                        mean_reward_max_in = mean_reward
+                
+                if (mean_reward_max_in / (c_distance + c_entropy) / t_max) > mean_reward_max:
+                    mean_reward_max = mean_reward_max_in
+                    i_max = [t_max, c_distance, c_entropy]
+                    print(i_max)
+                        
+                    if np.mean(batch_rewards)> 50:
+                        print("You Win!!! You may stop training now")
+                '''
+                if counter == 3 and found == 0 and stop_counter < 6:
+                    counter = 2
+                    stop_counter += 1
+                counter = counter + 1
+            
+            print('Case: ',Case,'Run: ',Run,'Comig: ',num_COMIGs)
+            best_score = -10000
+            best_index = -10000
+            for it in range(comigs_each_run):
+                coef_temp = good[num_COMIGs,it]
+                conflict_current, _, _ = accuracy(coef_temp.reshape(-1,1))
+                coeffs_strength = np.sum(np.abs(coef_temp))
+                coeffs_number = np.sum(coef_temp != 0)
+                score_current = c_accuracy*(1 - conflict_current) - c_strength*coeffs_strength - c_num_of_terms*coeffs_number
+                if score_current > best_score:
+                    best_score = score_current
+                    best_index = it
+            good_ind.append(best_index)
+            score = score + best_score
+        print('num of comigs = ',num_COMIGs)
+        if num_COMIGs < best_num_of_comigs:
+            best_num_of_comigs = num_COMIGs
+            Best_score = score
+            savemat('data' + str(Case) + '.mat',{'coef':coef[:num_COMIGs]})
+        elif num_COMIGs == best_num_of_comigs:
+            if score > Best_score:
+                savemat('data' + str(Case) + '.mat',{'coef':coef[:num_COMIGs]})
+    
+    print('Done!')
+    print('best num of comigs = ',best_num_of_comigs,'\n')
 
---
+++ everything_else/codes/test/pretrain.m
@@ -0,0 +1,119 @@
+function [input,LHS,allbits,reset_state] = pretrain(Case,n,aux)
+    coeffs_range = -1:1;
+    allCombos = dec2bin(0:2^n-1) -'0';
+
+    b = cell(n,1);
+    for i = 1:n
+        b{i} = allCombos(:,i);
+    end
+
+    switch Case
+        case 0
+            LHS = b{1}.*b{2}.*b{3}.*b{4} + b{2}.*b{3}.*b{4}.*b{5} + b{3}.*b{4}.*b{5}.*b{6} + b{4}.*b{5}.*b{6}.*b{7};
+        case 1
+            LHS = b{1}.*b{2}.*b{3}.*b{4} + b{2}.*b{3}.*b{4}.*b{5} + b{3}.*b{4}.*b{5}.*b{6} - b{4}.*b{5}.*b{6}.*b{7};
+        case 2
+            LHS = b{1}.*b{2}.*b{3}.*b{4} - b{2}.*b{3}.*b{4}.*b{5} - b{3}.*b{4}.*b{5}.*b{6} + b{4}.*b{5}.*b{6}.*b{7};
+    end
+    %LHS = b{1}.*b{2}.*b{3}.*b{4} + b{4}.*b{5}.*b{6}.*b{7};
+    LHS = LHS';
+    if aux == 1
+        LHS = LHS(2:2:2^n);
+    elseif aux == 2
+        LHS = LHS(4:4:2^n);
+    end
+
+    allbits = [];
+    for i = 1:n
+        for j = i+1:n
+            allbits = [allbits b{i}.*b{j}];
+        end
+    end
+
+    for i = 1:n
+        allbits = [allbits b{i}];
+    end
+	allbits = [allbits ones(2^n,1)];
+    allbits = allbits';
+
+    coeffs_size = n*(n+1)/2 + 1;
+    base = size(coeffs_range,2);
+    init = int2str( (base-1)/2 );
+
+    restartId = 0;
+    perCheck = 100000;
+    
+    data_percentage = 1;
+    conflicts_threshold = 60;
+    
+    data_size = floor(data_percentage*base^coeffs_size);
+    progress_const = 100 * perCheck / data_size;
+    good_coeffs = [];
+    good_prcntg = [];
+    good_count = 0;
+    t = cputime;
+    t_init = t;
+    for checkpoint = restartId : floor( (data_size-1)/perCheck )
+        k = int64(checkpoint*perCheck) : min( floor(data_size-1), int64((checkpoint+1)*perCheck) - 1 );
+        %k = randperm( data_size-1 , perCheck); % use this for random sample
+        coeffs = ndec2base(k,base,coeffs_size) - '0';%init;
+        coeffs( coeffs == 2 ) = -1;
+
+        RHS = coeffs*allbits;
+        if aux == 1
+            RHS = min(RHS(:,1:2:2^n-1),RHS(:,2:2:2^n));
+        elseif aux == 2
+            min1 = min(RHS(:,1:4:2^n-1),RHS(:,2:4:2^n));
+            min2 = min(RHS(:,3:4:2^n-1),RHS(:,4:4:2^n));
+            RHS = min(min1,min2);
+        end
+
+        %RHS = RHS - min(RHS,[],2);
+
+        conflicts_percent = mean( RHS ~= LHS , 2 ) * 100; % percentage of overall conflicts
+        index_good = (conflicts_percent <= conflicts_threshold);
+
+        %input(checkpoint*perCheck+1:min( floor(data_size), int64((checkpoint+1)*perCheck)),:) = coeffs; %for sampling
+        %target(checkpoint*perCheck+1:min( floor(data_size), int64((checkpoint+1)*perCheck))) = index_good; %for sampling
+        if any(index_good)
+            good_coeffs = [good_coeffs; coeffs(index_good,:)];
+            good_prcntg = [good_prcntg; conflicts_percent(index_good)];
+            good_count = good_count + sum(index_good);
+        end
+        %{
+        if mod(checkpoint,100) == 0
+            fprintf('progress %.4f%%, restart id = %d, step time = %.3f, total time = %.3f, good = %d\n',...
+                min((checkpoint+1)*progress_const, 100), checkpoint,cputime - t,cputime - t_init,good_count);
+            t = cputime;
+        end
+        %}
+        if (size(good_coeffs,1) > 10000) || (cputime-t_init > 90)
+            break;
+        end
+    end
+
+    %fprintf('progress %.3f%%, restart id = %d, step time = %.3f, total time = %.3f, good = %d\n',...
+    %    min((checkpoint+1)*progress_const, 100), checkpoint,cputime - t,cputime - t_init,good_count);
+
+    input = good_coeffs(randperm(size(good_coeffs,1),min(1000,floor(0.7*size(good_coeffs,1)))),:);
+    RHS = good_coeffs*allbits;
+    if aux == 1
+        RHS = min(RHS(:,1:2:2^n-1),RHS(:,2:2:2^n));
+    elseif aux == 2
+        min1 = min(RHS(:,1:4:2^n-1),RHS(:,2:4:2^n));
+        min2 = min(RHS(:,3:4:2^n-1),RHS(:,4:4:2^n));
+        RHS = min(min1,min2);
+    end
+    %const_term = -min(RHS,[],2);
+    %RHS = RHS + const_term;
+
+    accuracy_percent = mean( RHS == LHS , 2 ) * 100; % percentage of overall conflicts
+    index_good = (accuracy_percent >= 60);
+    if sum(index_good) == 0
+        index_good = (accuracy_percent >= 40);
+    end
+
+    temp = good_coeffs(index_good,:);
+    reset_state = temp(randperm(size(temp,1),1),:);
+    %save('data.mat','input','LHS','allbits','reset_state');
+end
