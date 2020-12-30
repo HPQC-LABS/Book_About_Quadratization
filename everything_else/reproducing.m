92a15307f47e9bbbdbf55f5bf0ec3d41e6b73c1e
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2020-12-29 23:43:30 -0500

    Update reproducing.m

--- everything_else/reproducing.m
+++ everything_else/reproducing.m
@@ -5,6 +5,10 @@ b1=b(:,1);b2=b(:,2);b3=b(:,3);b4=b(:,4);
 LHS=b1.*b2 + b2.*b3 + b3.*b4 - 4*b1.*b2.*b3;
 RHS=b1.*b2 + b2.*b3 + b3.*b4 + 4*b1 - 4*b1.*b2 - 4*b1.*b3;
 
+%% Pg. 3, Eqs 9 and 11
+LHS=b1.*b2 + b2.*b3 + b3.*b4 - 4*b1.*b2.*b3;
+RHS=b1.*b2 + b2.*b3 + b3.*b4 + 4*b1 - 4*b1.*b2 - 4*b1.*b3;
+
 %% Pg. 1, Eqs 3-4 (see abstract for AQC 2019, the code is in there, might also be saved on computer)
 
 z=[1 0; 0 -1];
