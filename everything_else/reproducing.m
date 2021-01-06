ba10eb3fc2570109d6da2d1b6d2b1c8eafe0de70
Nike Dattani <dattani.nike@gmail.com>
2021-01-05 19:27:44 -0500

    Did two things:
    1) Added close bracket for RBS-Rosenberg in reproducing.m
    2) Added PTR-KZ verification to the reproducing.m

--- everything_else/reproducing.m
+++ everything_else/reproducing.m
@@ -85,7 +85,7 @@ RHS=4*za+2*(z1+z2+z3+z4)+4*za*(z1+z2+z3+z4)+2*(z1*z2+z1*z3+z1*z4+z2*z3+z2*z4+z3*
 
 [diag(LHS) diag(RHS)];
 
-%% RBS-Rosenberg: b1b2b3 = min_ba(b1ba + b2b3 -2b2ba -2b3ba + 3ba
+%% RBS-Rosenberg: b1b2b3 = min_ba(b1ba + b2b3 -2b2ba -2b3ba + 3ba)
 
 b=[0 1];
 b1=kron(b,ones(1,8));
@@ -95,3 +95,9 @@ ba=kron(ones(1,8),b);
 
 LHS=b1.*b2.*b3;
 RHS=b1.*ba + b2.*b3 -2*b2.*ba - 2*b3.*ba + 3*ba;
+
+%% PTR-KZ: b1b2b3 = min_ba(1 − (ba + b1 + b2 + b3) + ba (b1 + b2 + b3) + b1b2 + b1b3 + b2b3)
+
+LHS=min(reshape(b1.*b2.*b3,2,[]));
+RHS=min(reshape(1 - (b4+b1+b2+b3) + b4.*(b1+b2+b3) +b1.*b2+b1.*b3+b2.*b3,2,[]));
+isequal(LHS,RHS);
