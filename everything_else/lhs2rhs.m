8d6d41710cd39c726e98d73e88bd5ebddd633432
Tim Li <77031993+tl355@users.noreply.github.com>
2021-02-01 13:04:22 -0700

    added ZZZ-TI-CBBK to lhs2rhs.m

--- everything_else/lhs2rhs.m
+++ everything_else/lhs2rhs.m
@@ -1,5 +1,5 @@
 function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
-% test DC1, DC2, and KKR
+% test DC1, DC2, KKR, and ZZZ-TI-CBBK
 % operators shold be in the form of 'xyz'
 %
 % e.g.    [LHS, RHS] = lhs2rhs('xyz',10,'P(3->2)-DC2')
@@ -76,6 +76,33 @@ function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
 
         LHS = S{1}*S{2}*S{3};
         RHS = alpha*eye(2^(n+3)) + alpha_ss*(S{1}^2 + S{2}^2 + S{3}^2) + alpha_sx*(S{1}*xa1 + S{2}*xa2 + S{3}*xa3) + alpha_zz*(za1*za2 + za1*za3 + za2*za3);
+        
+   elseif strcmp(name_of_quadratization, 'ZZZ-TI-CBBK')
+        assert(n == 3, 'ZZZ-TI-CBBK requires a 3-local term, please only give 3 operators.');
+        for ind = 1:n
+            assert(operators(ind) == 'z', 'ZZZ-TI-CBBK requires a ZZZ term.');
+            S{ind} = kron(kron(eye(2^(ind-1)),z),eye(2^(n+1-ind)));
+        end
+        
+        xa = kron(eye(8),x);
+        za = kron(eye(8),z);
+        
+        alpha = 1;
+        alpha_I = (1/2)*(delta + ((alpha/6)^(2/5))*(delta^(3/5)) + 6*((alpha/6)^(4/5))*(delta^(1/5)) );
+        alpha_zi = (-1/2)*(( ((7/6)*alpha) + ( ((alpha/6)^(3/5))*(delta^(2/5))) ) - ( (alpha/6)*(delta^4) )^(1/5));
+        alpha_zj = alpha_zi;
+        alpha_zk = alpha_zi;
+        alpha_za = (-1/2)*( delta - (((alpha/6)^(2/5))*(delta^(3/5))) );
+        alpha_xa = ( (alpha/6)*(delta^4) )^(1/5);
+        alpha_zzia = (-1/2)*( ( (7/6)*alpha + ((alpha/6)^(3/5))*(delta^(2/5)) ) + ( (alpha/6)*(delta^4) )^(1/5) );
+        alpha_zzja = alpha_zzia;
+        alpha_zzka = alpha_zzja;
+        alpha_zzij = 2*((alpha/6)^(4/5))*((delta)^(1/5));
+        alpha_zzik = alpha_zzij;
+        alpha_zzjk = alpha_zzij;
+        
+        LHS = S{1}*S{2}*S{3};
+        RHS = alpha_I*eye(16) + alpha_zi*S{1} + alpha_zj*S{2} + alpha_zk*S{3} + alpha_za*za + alpha_xa*xa + alpha_zzia*S{1}*za + alpha_zzja*S{2}*za + alpha_zzka*S{3}*za + alpha_zzij*S{1}*S{2} + alpha_zzik*S{1}*S{3} + alpha_zzjk*S{2}*S{3};
 
     else
         disp('cannot find this method');
