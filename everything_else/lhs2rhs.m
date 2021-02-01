8f31127beaa024d85730e10fa08afc0b854e21d6
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-01 18:46:03 -0500

    Update lhs2rhs.m
    - Assign Delta to the number decided by a user directly
    - Change all delta to upper case to match description in the book
    - Add a sample test for eigenvectors and eigenvalues comparison using P(3->2)DC2

--- everything_else/lhs2rhs.m
+++ everything_else/lhs2rhs.m
@@ -1,12 +1,11 @@
-function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
+function [LHS, RHS] = lhs2rhs(operators, Delta, name_of_quadratization)
 % test DC1, DC2, KKR, and ZZZ-TI-CBBK
 % operators shold be in the form of 'xyz'
 %
-% e.g.    [LHS, RHS] = lhs2rhs('xyz',10,'P(3->2)-DC2')
-%         refers to quadratize x1*y2*z3 using P(3->2)-DC2 with delta = 1e10
+% e.g.    [LHS, RHS] = lhs2rhs('xyz',1e10,'P(3->2)-DC2')
+%         refers to quadratize x1*y2*z3 using P(3->2)-DC2 with Delta = 1e10
 
     n = strlength(operators);
-    delta = 10^(delta);
     S = cell(n);
     x = [0 1 ; 1 0]; y = [0 -1i ; 1i 0]; z = [1 0 ; 0 -1];
 
@@ -23,12 +22,12 @@ function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
         end
         xa = kron(eye(8),x); za = kron(eye(8),z);
 
-        alpha = (1/2)*delta;
-        alpha_s = ((1/4)*(delta^(2/3)) - 1);
-        alpha_z = (1/2)*delta;
-        alpha_ss = delta^(1/3);
-        alpha_sz = (1/4)*(delta^(2/3));
-        alpha_sx = delta^(2/3);
+        alpha = (1/2)*Delta;
+        alpha_s = ((1/4)*(Delta^(2/3)) - 1);
+        alpha_z = (1/2)*Delta;
+        alpha_ss = Delta^(1/3);
+        alpha_sz = (1/4)*(Delta^(2/3));
+        alpha_sx = Delta^(2/3);
 
         LHS = S{1}*S{2}*S{3};
         RHS = alpha*eye(16) + alpha_s*S{3} + alpha_z*za + alpha_ss*((S{1} + S{2})^2) + alpha_sx*(S{1}*xa + S{2}*xa) + alpha_sz*za*S{3};
@@ -47,10 +46,10 @@ function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
         xa1 = kron(kron(eye(8),x),eye(4)); xa2 = kron(kron(eye(16),x),eye(2)); xa3 = kron(eye(32),x);
         za1 = kron(kron(eye(8),z),eye(4)); za2 = kron(kron(eye(16),z),eye(2)); za3 = kron(eye(32),z);
 
-        alpha = -(1/8)*(delta);    % or they should be the same as DC1 (need to be tested)
-        alpha_ss = -(1/6)*(delta)^(1/3);
-        alpha_sx = (1/6)*(delta)^(2/3);
-        alpha_zz = (1/24)*(delta);
+        alpha = -(1/8)*(Delta);    % or they should be the same as DC1 (need to be tested)
+        alpha_ss = -(1/6)*(Delta)^(1/3);
+        alpha_sx = (1/6)*(Delta)^(2/3);
+        alpha_zz = (1/24)*(Delta);
 
         LHS = S{1}*S{2}*S{3};
         RHS = alpha*eye(2^(n+3)) + alpha_ss*(S{1}^2 + S{2}^2 + S{3}^2) + alpha_sx*(S{1}*xa1 + S{2}*xa2 + S{3}*xa3) + alpha_zz*(za1*za2 + za1*za3 + za2*za3);
@@ -69,38 +68,38 @@ function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
         xa1 = kron(kron(eye(8),x),eye(4)); xa2 = kron(kron(eye(16),x),eye(2)); xa3 = kron(eye(32),x);
         za1 = kron(kron(eye(8),z),eye(4)); za2 = kron(kron(eye(16),z),eye(2)); za3 = kron(eye(32),z);
 
-        alpha = (1/8)*delta;
-        alpha_ss = (1/6)*(delta)^(1/3);
-        alpha_sx = (-1/6)*(delta)^(2/3);
-        alpha_zz = (-1/24)*delta;
+        alpha = (1/8)*Delta;
+        alpha_ss = (1/6)*(Delta)^(1/3);
+        alpha_sx = (-1/6)*(Delta)^(2/3);
+        alpha_zz = (-1/24)*Delta;
 
         LHS = S{1}*S{2}*S{3};
         RHS = alpha*eye(2^(n+3)) + alpha_ss*(S{1}^2 + S{2}^2 + S{3}^2) + alpha_sx*(S{1}*xa1 + S{2}*xa2 + S{3}*xa3) + alpha_zz*(za1*za2 + za1*za3 + za2*za3);
-        
+
    elseif strcmp(name_of_quadratization, 'ZZZ-TI-CBBK')
         assert(n == 3, 'ZZZ-TI-CBBK requires a 3-local term, please only give 3 operators.');
         for ind = 1:n
             assert(operators(ind) == 'z', 'ZZZ-TI-CBBK requires a ZZZ term.');
             S{ind} = kron(kron(eye(2^(ind-1)),z),eye(2^(n+1-ind)));
         end
-        
+
         xa = kron(eye(8),x);
         za = kron(eye(8),z);
-        
+
         alpha = 1;
-        alpha_I = (1/2)*(delta + ((alpha/6)^(2/5))*(delta^(3/5)) + 6*((alpha/6)^(4/5))*(delta^(1/5)) );
-        alpha_zi = (-1/2)*(( ((7/6)*alpha) + ( ((alpha/6)^(3/5))*(delta^(2/5))) ) - ( (alpha/6)*(delta^4) )^(1/5));
+        alpha_I = (1/2)*(Delta + ((alpha/6)^(2/5))*(Delta^(3/5)) + 6*((alpha/6)^(4/5))*(Delta^(1/5)) );
+        alpha_zi = (-1/2)*(( ((7/6)*alpha) + ( ((alpha/6)^(3/5))*(Delta^(2/5))) ) - ( (alpha/6)*(Delta^4) )^(1/5));
         alpha_zj = alpha_zi;
         alpha_zk = alpha_zi;
-        alpha_za = (-1/2)*( delta - (((alpha/6)^(2/5))*(delta^(3/5))) );
-        alpha_xa = ( (alpha/6)*(delta^4) )^(1/5);
-        alpha_zzia = (-1/2)*( ( (7/6)*alpha + ((alpha/6)^(3/5))*(delta^(2/5)) ) + ( (alpha/6)*(delta^4) )^(1/5) );
+        alpha_za = (-1/2)*( Delta - (((alpha/6)^(2/5))*(Delta^(3/5))) );
+        alpha_xa = ( (alpha/6)*(Delta^4) )^(1/5);
+        alpha_zzia = (-1/2)*( ( (7/6)*alpha + ((alpha/6)^(3/5))*(Delta^(2/5)) ) + ( (alpha/6)*(Delta^4) )^(1/5) );
         alpha_zzja = alpha_zzia;
         alpha_zzka = alpha_zzja;
-        alpha_zzij = 2*((alpha/6)^(4/5))*((delta)^(1/5));
+        alpha_zzij = 2*((alpha/6)^(4/5))*((Delta)^(1/5));
         alpha_zzik = alpha_zzij;
         alpha_zzjk = alpha_zzij;
-        
+
         LHS = S{1}*S{2}*S{3};
         RHS = alpha_I*eye(16) + alpha_zi*S{1} + alpha_zj*S{2} + alpha_zk*S{3} + alpha_za*za + alpha_xa*xa + alpha_zzia*S{1}*za + alpha_zzja*S{2}*za + alpha_zzka*S{3}*za + alpha_zzij*S{1}*S{2} + alpha_zzik*S{1}*S{3} + alpha_zzjk*S{2}*S{3};
 
@@ -109,3 +108,35 @@ function [LHS, RHS] = lhs2rhs(operators, delta, name_of_quadratization)
         LHS = []; RHS = [];
     end
 end
+
+%% Below are some tests to compare eigenvalues and eigenvectors.
+%% If want to run it locally, please use Ctrl+T (or Cmd+T) to uncomment it, then use Ctrl+C to copy the test.
+%% Dont forget to use Ctrl+/ to comment it out again.
+
+%% test: P(3->2)DC2    also tested ('xxx',1e13,'P(3->2)DC2') ('xzx',1e10,'P(3->2)DC2') successfully
+% [LHS RHS] = lhs2rhs('zxx',1e10,'P(3->2)DC2');
+% [VR, ER] = eig(RHS); [VL, EL] = eig(LHS);
+
+%% Because the sign of eigenvectors does not matter, we set any non-zero elements in the vectors to 1.
+% ER = round(ER(:,1:8)); VR = +(round(VR(:,1:8)) ~= 0); VL = +(VL ~= 0);
+
+%% It is divided into two parts as the ground state and the first excited state to ensure the eigenvalues (energy) match.
+%% Sort VL one by one through finding the eigenvector that is closest to VR.
+% for ind_R = 1:4
+%    for ind_L = 1:8
+%      dist(ind_L,ind_R) = sqrt(dot((VR(:,ind_R)-VL(:,ind_L)),(VR(:,ind_R)-VL(:,ind_L))));
+%    end
+%    [dist(size(VL,2)+1,ind_R), ind] = min(dist(1:8,ind_R)); S_VL(:,ind_R) = VL(:,ind); S_EL(:,ind_R) = sum(EL(:,ind));
+% end
+
+% for ind_R = 5:8
+%    for ind_L = 9:16
+%       dist(ind_L,ind_R) = sqrt(dot((VR(:,ind_R)-VL(:,ind_L)),(VR(:,ind_R)-VL(:,ind_L))));
+%    end
+%    [dist(size(VL,2)+1,ind_R), ind] = min(dist(9:16,ind_R)); S_VL(:,ind_R) = VL(:,ind+8); S_EL(:,ind_R) = sum(EL(:,ind+8));
+% end
+
+% dist = transpose(dist(size(dist,1),:));
+% isequal(S_VL,VR);
+% isequal(S_EL,sum(ER));
+%% dist returns a zero vector because the eigenvectors are reproduced nicely, and S_EL is the corresponding eigenvalues of S_VL.
