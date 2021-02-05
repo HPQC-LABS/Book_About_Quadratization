4dddfcea89d7e64cc60370a6636d8020be582f1d
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-04 20:48:47 -0500

    Update testingPerturbativeGadgets.m
    - Update the eigenvalues and eigenvectors test for DC2

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,23 +1,22 @@
-%% DC2
-[LHS,RHS] = lhs2rhs('zzz',1,'P(3->2)DC2');
-[V_RHS, E_RHS] = eig(RHS); E_RHS=diag(E_RHS)';
-[V_LHS, E_LHS] = eig(LHS); E_LHS=diag(E_LHS)';
+%% DC1: zzz still failed, max(min(dist)) always equals to 1.4142 even when all 16 energies match
+%% KKR: failed for vectors comparison as well
 
-k=3;
-E_RHS = round(E_RHS(:,1:2^k)); % rounding just to make 1 and 1.0000 take the same amount of space, but beware that 0.5 will erroneously look the same as 1.
-V_RHS = V_RHS(:,1:2^k);        % looking at the eigenvectors with the lowest 8 eigenvalues.
+%% DC2:
+%% has tested zzz, zxx (4e12), zxz (4.5e12) etc., but need loop to find appropriate and more precise Delta value for zzz, zzx etx.
 
-dist=zeros(2^k,2^(k-1));
-for ind_R = 1:2^(k-1)          % We go up to 4 because any single 3-body term of Pauli operators will have 4 eigenvalues of -1 and 4 eigenvalues of +1.
-  for ind_L = 1:2^k
-    dist(ind_L,ind_R) = norm(V_RHS(:,ind_R)-V_LHS(:,ind_L));
-  end
+[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2'); restriction = 1e-03;
+[V_RHS,E_RHS] = eig(RHS); [V_LHS,E_LHS] = eig(LHS); E_RHS = diag(E_RHS)'; E_LHS = diag(E_LHS)';
+E_diff = abs(bsxfun(@minus,E_RHS,E_LHS'));  % find matching eigenvalues by calculating differences between every elements in E_RHS and E_LHS
+[ind_LHS, ind_RHS] = find(E_diff < restriction); % that tells us which columns of E_LHS and E_RHS match, respectively
+
+% unsolved problem: we use abs() here to limit the impact of a vector's sign, but it does not help for those need scalar multiplication (e.g. [0 0 2 0] and [0 0 1 0])
+dist = 2*ones(max(ind_LHS),max(ind_RHS)); % set dist to 2 defaultly so that we can find matching eigenvectors easily in next step
+for ind = 1:size(ind_LHS,1)               % calculate the distance between two eigenvectors whose eigevalues match, and return the result to corresponding cell
+    dist(ind_LHS(ind),ind_RHS(ind)) = norm(abs(V_RHS(:,ind_RHS(ind)))-abs(V_LHS(:,ind_LHS(ind)))); % rows of dist -> columns of V_LHS, columns of dist -> columns of V_RHS
 end
-[row, column]=find(dist<1e-13); % Rows and columns of dist, which have elements smaller than 1e-6 (i.e., rows tells us which columns of V_LHS are the same as the first 4 columns of V_RHS)
-sorted_V_LHS=V_LHS(:,row);
-sorted_E_LHS=E_LHS(row);
-isequal(sorted_V_LHS,V_RHS(:,1:2^(k-1))) % works for zzz, zzx, zzy, etc. but doesn't work for xxx, zxx, etc. Also, seems NOT to work for Delta > 1.
-isequal(sorted_E_LHS,E_RHS(:,1:2^(k-1))) % only the first two elements match, if Delta is only 1. 
 
-%% DC1
-%% KKR
+% dist_ind_LHS and dist_ind_RHS refers to rows and columns of dist, which have elements smaller than 1e-3 (i.e., dist_ind_LHS and dist_ind_RHS tells us which columns of V_LHS match the columns of V_RHS)
+[dist_ind_LHS, dist_ind_RHS] = find(dist < restriction);
+max(min(dist(:,min(ind_RHS):max(ind_RHS)))); % farthest distance between two matching eigenvectors is 7.3682e-05
+sorted_V_LHS = V_LHS(:,dist_ind_LHS);
+sorted_E_LHS = E_LHS(dist_ind_LHS);
