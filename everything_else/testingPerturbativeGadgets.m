a87e9f65395b898b7037a5f9750c7bd303d1c3ec
Nike Dattani <dattani.nike@gmail.com>
2021-02-05 19:13:52 -0500

    Many more changes to testingPerturbativeGadgets.m: - removed need for loop alogether - removed need for bsxfun altogether - shortened number of lines significantly - simplified comments and number of comments significantly - separated multi-command lines such that each line is doing only 1 command (now that the code is so simple) - this code needs to be improved to work for the case where eigenectors are not the same but just "close" to each other (Nike emailed Ruby about this just now).

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,19 +1,15 @@
-[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2'); tol = 1e-03;
-[V_RHS,E_RHS] = eig(RHS); [V_LHS,E_LHS] = eig(LHS); 
-E_RHS = diag(E_RHS); E_LHS = diag(E_LHS);
-E_diff = abs(bsxfun(@minus,E_RHS',E_LHS));       % find matching eigenvalues by calculating differences between every elements in E_RHS and E_LHS[
-[ind_LHS, ind_RHS] = find(E_diff < tol);         % that tells us which elements of E_LHS and E_RHS match, respectively
-
-% unsolved problem: we use abs() here to limit the impact of a vector's sign, but it does not help for those need scalar multiplication (e.g. [0 0 2 0] and [0 0 1 0])
-dist = 2*ones(max(ind_LHS),max(ind_RHS));        % set dist to 2 by default so that we can find matching eigenvectors easily in next step
-for ind = 1:size(ind_LHS,1)                      % calculate the distance between two eigenvectors whose eigevalues match, and return the result to corresponding cell
-    dist(ind_LHS(ind),ind_RHS(ind)) = norm(abs(V_RHS(:,ind_RHS(ind)))-abs(V_LHS(:,ind_LHS(ind)))); % rows of dist -> columns of V_LHS, columns of dist -> columns of V_RHS
-end
-
-[dist_ind_LHS, dist_ind_RHS] = find(dist < tol); % dist_ind_LHS and dist_ind_RHS refers to rows and columns of dist, which have elements smaller than 1e-3 (i.e., dist_ind_LHS and dist_ind_RHS tells us which columns of V_LHS match the columns of V_RHS)
-max(min(dist(:,min(ind_RHS):max(ind_RHS))));     % farthest distance between two matching eigenvectors is 7.3682e-05
-sorted_V_LHS = V_LHS(:,dist_ind_LHS);
-sorted_E_LHS = E_LHS(dist_ind_LHS);
+[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2'); 
+tol = 1e-03;
+[V_RHS,E_RHS] = eig(RHS); 
+[V_LHS,E_LHS] = eig(LHS); 
+E_RHS = diag(E_RHS); 
+E_LHS = diag(E_LHS);
+[ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
+L=V_LHS(:,ind_evals_L);                                                    % L = LHS eigenvectors for eigenvalues matching RHS
+R=V_RHS(:,ind_evals_R);
+[ind_evecs_L, ind_evecs_R] = find(L'*R);                                   % indices of L with matiching eigenvectors in R
+sorted_L=L(:,indL); 
+isequal(sorted_L(:,1:4),round(abs(V_RHS(:,1:4))))                          % gives 1
 
 %% Test results
 
