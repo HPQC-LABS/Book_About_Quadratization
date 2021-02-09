f33130749decec702dbf9da01ff6b5231afe868b
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-08 20:00:40 -0500

    Update testingPerturbativeGadgets.m
    - Add condition when matching eigenvectors to get rid of errors
    - Correct sorted_L, there is no indL variable
    - Select appropriate columns from sorted_L to compare with V_RHS, the original gave 0

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,15 +1,15 @@
-[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2'); 
+[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2');
 tol = 1e-03;
-[V_RHS,E_RHS] = eig(RHS); 
-[V_LHS,E_LHS] = eig(LHS); 
-E_RHS = diag(E_RHS); 
+[V_RHS,E_RHS] = eig(RHS);
+[V_LHS,E_LHS] = eig(LHS);
+E_RHS = diag(E_RHS);
 E_LHS = diag(E_LHS);
 [ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
-L=V_LHS(:,ind_evals_L);                                                    % L = LHS eigenvectors for eigenvalues matching RHS
-R=V_RHS(:,ind_evals_R);
-[ind_evecs_L, ind_evecs_R] = find(L'*R);                                   % indices of L with matiching eigenvectors in R
-sorted_L=L(:,indL); 
-isequal(sorted_L(:,1:4),round(abs(V_RHS(:,1:4))))                          % gives 1
+L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
+R = V_RHS(:,ind_evals_R);
+[ind_evecs_L, ind_evecs_R] = find( abs(L'*R) >= tol );                     % indices of L with matiching eigenvectors in R
+sorted_L = L(:,ind_evecs_L);
+isequal(sorted_L(:,1:32:end),round(abs(V_RHS(:,1:8))))                     % gives 1
 
 %% Test results
 
