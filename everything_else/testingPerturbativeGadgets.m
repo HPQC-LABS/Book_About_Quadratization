b1d1394882ec0109386370a4c2d3a7ebda4bfb0d
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-09 20:20:24 -0500

    Update testingPerturbativeGadgets.m
    - Use Euclidean distance to prove eigenvectors are matching instead of isequal() function
    - Add a failed example for xxz, which has more matching eigenvalues than eigenvectors

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -10,7 +10,9 @@ R = V_RHS(:,ind_evals_R);
 [ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
 sorted_L = L(:,ind_evecs_L);
 repeat_times = sum(ind_evals_R == ind_evals_R(1,1))*sum(ind_evecs_R == ind_evals_R(1,1));    % times of eigenvectors repeat in sorted_L
-isequal(sorted_L(:,1:repeat_times:end),round(abs(V_RHS(:,unique(ind_evals_R)))))                     % gives 1
+sorted_L = sorted_L(:,1:repeat_times:end);
+sorted_R = V_RHS(:,unique(ind_evals_R));
+max(sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) )) < tol;   % maximum distance between two matching eigenvectors is within the bound
 
 % Another potential solution: compute the distance of two eigenvectors
 
@@ -30,9 +32,9 @@ max(dist(ind_evecs))                        % gives the maximum distance of two 
 % KKR: failed for vectors comparison as well
 % DC2: We have tested zzz, zzx (3.58e12), zxz (4.5e12) etc., but need loop to find appropriate and more precise Delta value for zzz, zzx etx.
 
-% Failed example: it has 7 eigenvectors matching ( V_RHS(1-7) ), but still returns false due to small errors in V_RHS. We need to find a way to ignore those errors.
+% Successful example: it has 7 eigenvectors matching ( V_RHS(1-7) ), but still returns false due to small errors in V_RHS. We need to find a way to ignore those errors.
 
-[LHS,RHS] = lhs2rhs('zxz',4.5e12,'P(3->2)DC2');
+[LHS,RHS] = lhs2rhs('zzx',4.5e12,'P(3->2)DC2');
 tol = 1e-03;
 [V_RHS,E_RHS] = eig(RHS);
 [V_LHS,E_LHS] = eig(LHS);
@@ -43,5 +45,25 @@ L = V_LHS(:,ind_evals_L);                                                   % L 
 R = V_RHS(:,ind_evals_R);
 [ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
 sorted_L = L(:,ind_evecs_L);
-repeat_times = sum(ind_evals_R == ind_evals_R(1,1))*sum(ind_evecs_R == ind_evals_R(1,1));    % times of eigenvectors repeat in sorted_L
-isequal(sorted_L(:,1:repeat_times:end),round(abs(V_RHS(:,unique(ind_evals_R)))))
+repeat_times = sum(ind_evals_R == ind_evals_R(ind_evecs_R(1)))*sum(ind_evecs_R == ind_evecs_R(1));    % times of eigenvectors repeat in sorted_L
+sorted_L = sorted_L(:,1:repeat_times:end);  sorted_R = V_RHS(:,unique(ind_evals_R));
+dist = sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) );
+max(dist) < tol;   % gives 1
+
+% Failed example: it has 5 eigenvalues matching, but only has 1 matching eigenvectors. If there were more than 1 eigenvectors matching, it gives an error.
+
+[LHS,RHS] = lhs2rhs('xxz',4.3e12,'P(3->2)DC2');
+tol = 1e-03;
+[V_RHS,E_RHS] = eig(RHS);
+[V_LHS,E_LHS] = eig(LHS);
+E_RHS = diag(E_RHS);
+E_LHS = diag(E_LHS);
+[ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
+L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
+R = V_RHS(:,ind_evals_R);
+[ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
+sorted_L = L(:,ind_evecs_L);
+repeat_times = sum(ind_evals_R == ind_evals_R(ind_evecs_R(1)))*sum(ind_evecs_R == ind_evecs_R(1));    % times of eigenvectors repeat in sorted_L
+sorted_L = sorted_L(:,1:repeat_times:end);  sorted_R = V_RHS(:,unique(ind_evals_R));
+dist = sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) );
+max(dist) < tol;   % gives 1
