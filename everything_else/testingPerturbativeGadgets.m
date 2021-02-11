0da72476d29039805ce2b2fdb9a5419aa29ae516
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-10 20:51:59 -0500

    Update testingPerturbativeGadgets.m
    - Add a loop for Delta value test to get appropriate Delta that meet our requirements
    - The odd elements in delta_required refer to the value of desired Delta such that eigenvalues can match
    - Remove previous example, and add several new test results

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,54 +1,48 @@
-[LHS,RHS] = lhs2rhs('zzz',2e13,'P(3->2)DC2');
-tol = 1e-03;
+delta_required = zeros(7,1); tol = 1e-03; k = 1;    % fundamental settings where k is the number of auxiliary qubit
+for Delta = 1:1e12:1e13
+[LHS,RHS] = lhs2rhs('zzz',Delta,'P(3->2)DC2');
 [V_RHS,E_RHS] = eig(RHS);
 [V_LHS,E_LHS] = eig(LHS);
 E_RHS = diag(E_RHS);
 E_LHS = diag(E_LHS);
 [ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
-L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
-R = V_RHS(:,ind_evals_R);
-[ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
-sorted_L = L(:,ind_evecs_L);
-repeat_times = sum(ind_evals_R == ind_evals_R(1,1))*sum(ind_evecs_R == ind_evals_R(1,1));    % times of eigenvectors repeat in sorted_L
-sorted_L = sorted_L(:,1:repeat_times:end);
-sorted_R = V_RHS(:,unique(ind_evals_R));
-max(sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) )) < tol;   % maximum distance between two matching eigenvectors is within the bound
+  if isempty(ind_evals_L) == 0        % matching eigenvalues exist
+    sorted_E_RHS = sort(E_RHS);
+    if (delta_required(1) == 0) & (sum( abs(E_LHS - sorted_E_RHS(1)) < tol ) >= 2^k) % value of Delta that let ground energy match
+        delta_required(1) = Delta;
+    end
+    if (delta_required(3) == 0) & (sum( abs(E_LHS - sorted_E_RHS(2^k + 1)) < tol ) >= 2^k) % value of Delta that let first excited energy match
+        delta_required(3) = Delta;
+    end
+    if (delta_required(5) == 0) & (numel(unique(ind_evals_R)) == 2^(k+2))     % value of Delta that let all 8 energies match
+        delta_required(5) = Delta;
+    end
 
-% Another potential solution: compute the distance of two eigenvectors
+    L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
+    R = V_RHS(:,ind_evals_R);
+    [ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
 
-dist = sqrt( sum( (abs(L)-abs(R)).^2 ) );
-ind_evecs = find(dist < tol);              % indices of L with mathcing eigenvectors in R
-sorted_L = L(:,ind_evecs);
-isequal(sorted_L,round(abs(V_RHS(:,unique(ind_evals_R)))))  % gives 1
-max(dist(ind_evecs))                        % gives the maximum distance of two matching eigenvectors
-
-% Because iseuqal() function cannot return true when two eigenvectors are only 'close' to each other but not the same,
-% we can use sorted_L and maximum distance to determine if the result is expected (i.e. look at the number of matching eigenvectors and their distance)
-
+    if isempty(ind_evecs_L) == 0      % matching eigenvectors exist
+      sorted_L = L(:,ind_evecs_L);
+      repeat_times = sum(ind_evals_R == ind_evals_R(1,1))*sum(ind_evecs_R == ind_evals_R(1,1));    % times of eigenvectors repeat in sorted_L
+      sorted_L = sorted_L(:,1:repeat_times:end);
+      sorted_R = V_RHS(:,unique(ind_evals_R));
+      if size(sorted_L,2) == size(sorted_R,2)
+          max(sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) )) < tol;   % maximum distance between two matching eigenvectors is within the bound
+      end
+    end
+  end
+  if ne(delta_required(5),0) & (delta_required(7) == 0) & (numel(unique(ind_evals_R)) == 0)     % value of Delta that is too large to keep energies matching
+      delta_required(7) = Delta;
+  end
+end
+delta_required(delta_required == 0) = nan;
 
 %% Test results
 
 % DC1: zzz still failed, max(min(dist)) always equals to 1.4142 even when all 16 energies match
 % KKR: failed for vectors comparison as well
-% DC2: We have tested zzz, zzx (3.58e12), zxz (4.5e12) etc., but need loop to find appropriate and more precise Delta value for zzz, zzx etx.
-
-% Successful example: it has 7 eigenvectors matching ( V_RHS(1-7) ), but still returns false due to small errors in V_RHS. We need to find a way to ignore those errors.
-
-[LHS,RHS] = lhs2rhs('zzx',4.5e12,'P(3->2)DC2');
-tol = 1e-03;
-[V_RHS,E_RHS] = eig(RHS);
-[V_LHS,E_LHS] = eig(LHS);
-E_RHS = diag(E_RHS);
-E_LHS = diag(E_LHS);
-[ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
-L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
-R = V_RHS(:,ind_evals_R);
-[ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
-sorted_L = L(:,ind_evecs_L);
-repeat_times = sum(ind_evals_R == ind_evals_R(ind_evecs_R(1)))*sum(ind_evecs_R == ind_evecs_R(1));    % times of eigenvectors repeat in sorted_L
-sorted_L = sorted_L(:,1:repeat_times:end);  sorted_R = V_RHS(:,unique(ind_evals_R));
-dist = sqrt( sum( (abs(sorted_L)-abs(sorted_R)).^2 ) );
-max(dist) < tol;   % gives 1
+% DC2: We have tested xxz (2.05e12), xxx (2.488e12), zyz (1.833e12), yyy (2.114e12) etc. successfully for eigenvalues matching.
 
 % Failed example: it has 5 eigenvalues matching, but only has 1 matching eigenvectors. If there were more than 1 eigenvectors matching, it gives an error.
 
