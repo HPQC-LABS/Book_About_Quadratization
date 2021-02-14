32899246dc41c51296263ca245f49b53130c48d3
Ruby Kong <74504036+RubyK777@users.noreply.github.com>
2021-02-14 16:29:06 -0500

    Update testingPerturbativeGadgets.m
    - Modify the Delta value test for eigenvalues and eigenvectors to match. Now it tests eigenvalues and eigenvectors at the same time.
    - Add an alternative test that use euclidean distance to find matching eigenvectors, which can avoid computing distance multiple times.

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,48 +1,50 @@
 delta_required = zeros(7,1); tol = 1e-03; k = 1;    % fundamental settings where k is the number of auxiliary qubit
-for Delta = 1:1e9:1e13
-[LHS,RHS] = lhs2rhs('zzz',Delta,'P(3->2)DC2');
+for Delta = 1e9:1e9:1e13
+[LHS,RHS] = lhs2rhs('xxx',Delta,'P(3->2)DC2');
 [V_RHS,E_RHS] = eig(RHS);
 [V_LHS,E_LHS] = eig(LHS);
-E_RHS = diag(E_RHS);
-E_LHS = diag(E_LHS);
+E_RHS = sort(diag(E_RHS));
+E_LHS = sort(diag(E_LHS));
 [ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
-  if isempty(ind_evals_L) == 0        % matching eigenvalues exist
-    sorted_E_RHS = sort(E_RHS);
-    if (sum( abs(E_LHS - sorted_E_RHS(1)) < tol ) >= 2^k) % value of Delta that let ground energy match
-        if (delta_required(1) == 0)
-            delta_required(1) = Delta;
-        end
-        if (sum( abs(E_LHS - sorted_E_RHS(2^k + 1)) < tol ) >= 2^k) % value of Delta that let first excited energy match
-            if (delta_required(3) == 0)
-                delta_required(3) = Delta;
-            end
-        end
-    end
-    if (delta_required(5) == 0) & (numel(unique(ind_evals_R)) == 2^(k+2))     % value of Delta that let all 8 energies match
-        delta_required(5) = Delta;
-    end
-
+if isempty(ind_evals_L) == 0        % matching eigenvalues exist
     L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
     R = V_RHS(:,ind_evals_R);
     [ind_evecs_L, ind_evecs_R] = find( abs(L'*R)-1 > -tol );                     % indices of L with matiching eigenvectors in R
-    sorted_L = L(:,ind_evecs_L);
-    if isempty(ind_evecs_L) == 0      % matching eigenvectors exist
-      sorted_L = unique(sorted_L','rows','stable')';   % remove repeated eigenvectors while maintaining the order
-      sorted_R = V_RHS(:,unique(ind_evals_R));
-      if (size(sorted_L,2) >= 2^k) & (delta_required(2) == 0) & (sum( sqrt( sum((abs(sorted_L(:,1:2^k))-abs(sorted_R(:,1:2^k))).^2) ) < tol ) == 2^k)  % value of Delta that let ground state match
-          delta_required(2) = Delta;
-      end
-      if (size(sorted_L,2) >= 2^(k+1)) & (delta_required(4) == 0) & (sum( sqrt( sum((abs(sorted_L(:,1:2^(k+1)))-abs(sorted_R(:,1:2^(k+1)))).^2) ) < tol ) == 2^(k+1))   % value of Delta that let first excited state match
-          delta_required(4) = Delta;
-      end
-      if isequal(size(sorted_L,2),size(sorted_R,2),2^(k+2)) & (delta_required(6) == 0) & (max(sqrt( sum((abs(sorted_L)-abs(sorted_R)).^2) )) < tol)  % value of Delta that let all 8 states match
-          delta_required(6) = Delta;
-      end
+    sorted_L = unique(L(:,ind_evecs_L)','rows','stable')';                       % remove repeated eigenvectors while maintaining the order
+    sorted_R = V_RHS(:,unique(ind_evals_R));
+
+    if (sum( abs(E_LHS(1:2^k) - E_RHS(1)) < tol ) == 2^k)
+        if (delta_required(1) == 0)
+            delta_required(1) = Delta;  % value of Delta that let ground energy match
+        end
+        if (isempty(ind_evecs_L) == 0) & (delta_required(2) == 0) & (norm( abs(sorted_L(:,1))-abs(sorted_R(:,1)) ) < tol)
+            delta_required(2) = Delta;  % value of Delta that let ground state match
+        end
+
+        if (sum( abs(E_LHS((2^k + 1):2^(k+1)) - E_RHS(2)) < tol ) == 2^k)
+            if (delta_required(3) == 0)
+                delta_required(3) = Delta;  % value of Delta that let first excited energy match
+            end
+
+            if (size(sorted_L,2) >= 2) & (delta_required(4) == 0) & (norm( abs(sorted_L(:,2))-abs(sorted_R(:,2))) < tol )
+                delta_required(4) = Delta;   % value of Delta that let first excited state match
+            end
+        end
+
+        if (numel(unique(ind_evals_R)) == 8)
+            if (delta_required(5) == 0)
+            delta_required(5) = Delta;   % value of Delta that let all 8 energies match
+            end
+
+            if isequal(size(sorted_L,2),size(sorted_R,2),8) & (delta_required(6) == 0) & (max( sqrt(sum( (abs(sorted_L)-abs(sorted_R)).^2 )) ) < tol)
+                delta_required(6) = Delta;  % value of Delta that let all 8 states match
+            end
+        end
     end
-  end
-  if ne(delta_required(5),0) & ne(delta_required(6),0) & (delta_required(7) == 0) & (numel(unique(ind_evals_R)) == 0)  % value of Delta that is too large to keep energies matching
-      delta_required(7) = Delta;
-  end
+end
+if ne(delta_required(5),0) & ne(delta_required(6),0) & (delta_required(7) == 0) & (numel(unique(ind_evals_R)) == 0)
+    delta_required(7) = Delta;  % value of Delta that is too large to keep energies matching
+end
 end
 delta_required(delta_required == 0) = nan;
 
@@ -54,3 +56,55 @@ delta_required(delta_required == 0) = nan;
 %     xxx(nan), xxz (2.06375e12), xxy(nan), xzx(nan), xyx(nan), zxx (6.769e12), yxx(nan)
 %     yyy(nan), zyy (6.5480e12), xyy(nan), yzy(nan), yxy(nan), yyx(nan), yyz(nan)
 %     xzy(nan), xyz(nan), zxy (6.0378e12), zyx (5.46549e12), yxz(nan), yzx(nan) for all eigenvectors and eigenvalues to match.
+
+
+%% Alternative: use euclidean distance to find matching eigenvectors
+
+delta_required = zeros(7,1); tol = 1e-03; k = 1;    % fundamental settings where k is the number of auxiliary qubit
+for Delta = 1e9:1e9:1e13
+[LHS,RHS] = lhs2rhs('xxx',Delta,'P(3->2)CBBK');
+[V_RHS,E_RHS] = eig(RHS);
+[V_LHS,E_LHS] = eig(LHS);
+E_RHS = sort(diag(E_RHS));
+E_LHS = sort(diag(E_LHS));
+[ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
+if isempty(ind_evals_L) == 0        % matching eigenvalues exist
+    L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
+    R = V_RHS(:,ind_evals_R);
+    ind_evecs = find( sqrt(sum( (abs(L)-abs(R)).^2 ) ) < tol);
+    sorted_L = unique(L(:,ind_evecs)','rows','stable')';                       % remove repeated eigenvectors while maintaining the order
+    sorted_R = unique(R(:,ind_evecs)','rows','stable')';
+    if (sum( abs(E_LHS(1:2^k) - E_RHS(1)) < tol ) == 2^k)
+            if (delta_required(1) == 0)
+                delta_required(1) = Delta;  % value of Delta that let ground energy match
+            end
+            if (isempty(ind_evecs) == 0) & (delta_required(2) == 0)
+                delta_required(2) = Delta;  % value of Delta that let ground state match
+            end
+
+            if (sum( abs(E_LHS((2^k + 1):2^(k+1)) - E_RHS(2)) < tol ) == 2^k)
+                if (delta_required(3) == 0)
+                    delta_required(3) = Delta;  % value of Delta that let first excited energy match
+                end
+
+                if (size(sorted_L,2) >= 2) & (delta_required(4) == 0)
+                    delta_required(4) = Delta;   % value of Delta that let first excited state match
+                end
+            end
+
+            if (numel(unique(ind_evals_R)) == 8)
+                if (delta_required(5) == 0)
+                delta_required(5) = Delta;   % value of Delta that let all 8 energies match
+                end
+
+                if isequal(size(sorted_L,2),size(sorted_R,2),8) & (delta_required(6) == 0)
+                    delta_required(6) = Delta;  % value of Delta that let all 8 states match
+                end
+            end
+    end
+end
+if ne(delta_required(5),0) & ne(delta_required(6),0) & (delta_required(7) == 0) & (numel(unique(ind_evals_R)) == 0)
+    delta_required(7) = Delta;  % value of Delta that is too large to keep energies matching
+end
+end
+delta_required(delta_required == 0) = nan;
