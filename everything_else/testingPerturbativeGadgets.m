82b623b03aa251756feb78d3e38ba6deabcb5897
RubyK777 <r7kong@uwaterloo.ca>
2021-02-25 23:35:05 -0500

    Update testingPerturbativeGadgets.m
    - Update previous samples to make it capable to run on Octave as well
    - Use a parfor loop to improve the efficiency of the tests, but it can only run on Octave due to a stricter rule using on MATLAB

--- everything_else/testingPerturbativeGadgets.m
+++ everything_else/testingPerturbativeGadgets.m
@@ -1,22 +1,25 @@
 delta_required = zeros(7,1); tol = 1e-03; k = 1;    % fundamental settings where k is the number of auxiliary qubit
-for Delta = 1e9:1e9:1e13
+parfor Delta = 1e9:1e9:1e13                         % this parfor loop is able to run on Octave, but not on MATLAB
 [LHS,RHS] = lhs2rhs('xxx',Delta,'P(3->2)CBBK');
 [V_RHS,E_RHS] = eig(RHS);
 [V_LHS,E_LHS] = eig(LHS);
-E_RHS = sort(diag(E_RHS));
-E_LHS = sort(diag(E_LHS));
+[E_RHS,index] = sort(diag(E_RHS)); V_RHS = V_RHS(:,index);
+[E_LHS,index] = sort(diag(E_LHS)); V_LHS = V_LHS(:,index);
 [ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
 if isempty(ind_evals_L) == 0        % matching eigenvalues exist
-    L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
+    L = V_LHS(:,ind_evals_L);                                               % L = LHS eigenvectors for eigenvalues matching RHS
     R = V_RHS(:,ind_evals_R);
     ind_evecs = find( sqrt(sum( (abs(L)-abs(R)).^2 ) ) < tol);
-    sorted_L = unique(L(:,ind_evecs)','rows','stable')';                       % remove repeated eigenvectors while maintaining the order
-    sorted_R = unique(R(:,ind_evecs)','rows','stable')';
+    L = L(:,ind_evecs);
+    [~,index] = unique(L','rows','first');
+    sorted_L = L(:,sort(index));                                            % remove repeated eigenvectors while maintaining the order
+    R = R(:,ind_evecs);
+    sorted_R = R(:,sort(index));
     if (sum( abs(E_LHS(1:2^k) - E_RHS(1)) < tol ) == 2^k)
             if (delta_required(1) == 0)
                 delta_required(1) = Delta;  % value of Delta that let ground energy match
             end
-            if (isempty(ind_evecs) == 0) & (delta_required(2) == 0)
+            if (isempty(ind_evecs) == 0) && (delta_required(2) == 0)
                 delta_required(2) = Delta;  % value of Delta that let ground state match
             end
 
@@ -25,7 +28,7 @@ if isempty(ind_evals_L) == 0        % matching eigenvalues exist
                     delta_required(3) = Delta;  % value of Delta that let first excited energy match
                 end
 
-                if (size(sorted_L,2) >= 2) & (delta_required(4) == 0)
+                if (size(sorted_L,2) >= 2) && (delta_required(4) == 0)
                     delta_required(4) = Delta;   % value of Delta that let first excited state match
                 end
             end
@@ -35,17 +38,18 @@ if isempty(ind_evals_L) == 0        % matching eigenvalues exist
                 delta_required(5) = Delta;   % value of Delta that let all 8 energies match
                 end
 
-                if isequal(size(sorted_L,2),size(sorted_R,2),8) & (delta_required(6) == 0)
+                if isequal(size(sorted_L,2),size(sorted_R,2),8) && (delta_required(6) == 0)
                     delta_required(6) = Delta;  % value of Delta that let all 8 states match
                 end
             end
     end
 end
-if ne(delta_required(5),0) & ne(delta_required(6),0) & (delta_required(7) == 0) & (numel(unique(ind_evals_R)) == 0)
+if ne(delta_required(5),0) && ne(delta_required(6),0) && (delta_required(7) == 0) && (numel(unique(ind_evals_R)) == 0)
     delta_required(7) = Delta;  % value of Delta that is too large to keep energies matching
 end
 end
 delta_required(delta_required == 0) = nan;
+delta_required
 
 %% Test results
 
@@ -61,27 +65,30 @@ S{1} = 'x'; S{2} = 'y'; S{3} = 'z'; n_combination = 1;   % fundamental settings 
 for s1 = 1:3
 for s2 = 1:3
 for s3 = 1:3
-    for i = -308:308       % very rough test from Delta = 1e-308 to Delta = 1e308
+    parfor i = -308:308     % this parfor loop is able to run on Octave, but not on MATLAB
     Delta = 10^i;
     combinations(n_combination) = {['-' S{s1} S{s2} S{s3}]};     % terms that being tested
     [LHS,RHS] = lhs2rhs(['-' S{s1} S{s2} S{s3}],Delta,'P(3->2)OT');
     if isnan(RHS) == 0
       [V_RHS,E_RHS] = eig(RHS);
       [V_LHS,E_LHS] = eig(LHS);
-      E_RHS = sort(diag(E_RHS));
-      E_LHS = sort(diag(E_LHS));
+      [E_RHS,index] = sort(diag(E_RHS)); V_RHS = V_RHS(:,index);
+      [E_LHS,index] = sort(diag(E_LHS)); V_LHS = V_LHS(:,index);
       [ind_evals_L, ind_evals_R] = find( abs(E_RHS'-E_LHS) < tol );              % indices of LHS and RHS where eigenvalues match within tol
       if isempty(ind_evals_L) == 0        % matching eigenvalues exist
         L = V_LHS(:,ind_evals_L);                                                   % L = LHS eigenvectors for eigenvalues matching RHS
         R = V_RHS(:,ind_evals_R);
         ind_evecs = find( sqrt(sum( (abs(L)-abs(R)).^2 ) ) < tol);
-        sorted_L = unique(L(:,ind_evecs)','rows','stable')';                       % remove repeated eigenvectors while maintaining the order
-        sorted_R = unique(R(:,ind_evecs)','rows','stable')';
+        L = L(:,ind_evecs);
+        [~,index] = unique(L','rows','first');
+        sorted_L = L(:,sort(index));                                            % remove repeated eigenvectors while maintaining the order
+        R = R(:,ind_evecs);
+        sorted_R = R(:,sort(index));
         if (sum( abs(E_LHS(1:2^k) - E_RHS(1)) < tol ) == 2^k)
               if (delta_required(1,n_combination) == 0)
                   delta_required(1,n_combination) = Delta;  % value of Delta that let ground energy match
               end
-              if (isempty(ind_evecs) == 0) & (delta_required(2,n_combination) == 0)
+              if (isempty(ind_evecs) == 0) && (delta_required(2,n_combination) == 0)
                   delta_required(2,n_combination) = Delta;  % value of Delta that let ground state match
               end
 
@@ -90,7 +97,7 @@ for s3 = 1:3
                       delta_required(3,n_combination) = Delta;  % value of Delta that let first excited energy match
                   end
 
-                  if (size(sorted_L,2) >= 2) & (delta_required(4,n_combination) == 0)
+                  if (size(sorted_L,2) >= 2) && (delta_required(4,n_combination) == 0)
                       delta_required(4,n_combination) = Delta;   % value of Delta that let first excited state match
                   end
               end
@@ -100,12 +107,12 @@ for s3 = 1:3
                     delta_required(5,n_combination) = Delta;   % value of Delta that let all 8 energies match
                   end
 
-                  if isequal(size(sorted_L,2),size(sorted_R,2),8) & (delta_required(6,n_combination) == 0)
+                  if isequal(size(sorted_L,2),size(sorted_R,2),8) && (delta_required(6,n_combination) == 0)
                       delta_required(6,n_combination) = Delta;  % value of Delta that let all 8 states match
                   end
               end
         end
-        if ne(delta_required(5,n_combination),0) & ne(delta_required(6,n_combination),0) & (delta_required(7,n_combination) == 0) & (numel(unique(ind_evals_R)) == 0)
+        if ne(delta_required(5,n_combination),0) && ne(delta_required(6,n_combination),0) && (delta_required(7,n_combination) == 0) && (numel(unique(ind_evals_R)) == 0)
           delta_required(7,n_combination) = Delta;  % value of Delta that is too large to keep energies matching
         end
       end
@@ -116,3 +123,5 @@ end
 end
 end
 delta_required(delta_required == 0) = nan;
+combinations
+delta_required     % get delta_required and corresponding combinations as outputs
