1b389019f737e98eaf271f3ed3a47bd8d8472a9e
RubyK777 <r7kong@uwaterloo.ca>
2021-03-09 21:37:12 -0500

    Update FindReqdDelta.m
    - Add an argument 'decimal_place' in order to control the precision of test
    - delta_required shows only the exponent of the Delta rather than the exact value
    - The function is able to print the delta_required and the largest Delta tested during executing now. The output file will be updated when Delta is 1eN or 5eN.

--- everything_else/FindReqdDelta.m
+++ everything_else/FindReqdDelta.m
@@ -1,13 +1,19 @@
-function delta_required = FindReqdDelta(tol,coefficient,input_choice,minDelta,maxDelta,name_of_quadratization)
+function delta_required = FindReqdDelta(tol,coefficient,decimal_palce,input_choice,minDelta,maxDelta,name_of_quadratization,test_times)
 % This function takes in 6 arguments and gives a double matrix as output.
 % It finds the Delta values that meet requriements stated below, in the range provide, and prints the output to a text file.
 %
-% e.g.    delta_required = FindReqdDelta(1e-3,-1,'xyz',1e10,1e13,'P(3->2)DC2')
-%         refers to find the Delta required within 1e10-1e13 for -xyz using DC2
+% e.g.    delta_required = FindReqdDelta(1e-3,-1,3,'xyz',1e10,1e13,'P(3->2)DC2',1)
+%         refers to find the Delta required within 1e10-1e13 (3 decimal places) for -xyz using DC2 the 1st time
 
 if strcmp(input_choice,"all_cubics") || strcmp(input_choice,"27_comb")     % test all 27 cubic terms
   delta_required = zeros(7,27); combinations = cell(1,27);
   S{1} = 'x'; S{2} = 'y'; S{3} = 'z'; n_combination = 1;
+
+  if coefficient == -1
+  FileName = strcat('negative','_',name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(test_times),'.txt');
+  else
+  FileName = strcat('positive','_',name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(test_times),'.txt');
+  end
 
   for s1 = 1:3
   for s2 = 1:3
@@ -21,7 +27,7 @@ if strcmp(input_choice,"all_cubics") || strcmp(input_choice,"27_comb")     % tes
 
   Delta = minDelta;
   while Delta <= maxDelta
-  Delta = Delta + 10^(floor(log10(Delta))-4);
+  Delta = Delta + 10^(floor(log10(Delta))-decimal_place);
   [LHS,RHS] = lhs2rhs(coefficient,operators,Delta,name_of_quadratization);
   if isnan(RHS) == 0
     m = log2(size(RHS,2)) - length(operators);             % the number of auxiliary qubits
@@ -41,58 +47,65 @@ if strcmp(input_choice,"all_cubics") || strcmp(input_choice,"27_comb")     % tes
       sorted_R = R(:,sort(index));
       if (sum( abs(E_LHS(1:2^m) - E_RHS(1)) < tol ) == 2^m)
           if (delta_required(1,n_combination) == 0)
-              delta_required(1,n_combination) = Delta;  % value of Delta that let ground energy match
+              delta_required(1,n_combination) = floor(log10(Delta));  % value of Delta that let ground energy match
           end
           if (isempty(ind_evecs) == 0) && (delta_required(2,n_combination) == 0)
-              delta_required(2,n_combination) = Delta;  % value of Delta that let ground state match
+              delta_required(2,n_combination) = floor(log10(Delta));  % value of Delta that let ground state match
           end
 
           if (sum( abs(E_LHS((2^m + 1):2^(m+1)) - E_RHS(2)) < tol ) == 2^m)
               if (delta_required(3,n_combination) == 0)
-                  delta_required(3,n_combination) = Delta;  % value of Delta that let first excited energy match
+                  delta_required(3,n_combination) = floor(log10(Delta));  % value of Delta that let first excited energy match
               end
 
               if (size(sorted_L,2) >= 2) && (delta_required(4,n_combination) == 0)
-                  delta_required(4,n_combination) = Delta;   % value of Delta that let first excited state match
+                  delta_required(4,n_combination) = floor(log10(Delta));   % value of Delta that let first excited state match
               end
           end
 
           if (numel(unique(ind_evals_R)) == 8)
               if (delta_required(5,n_combination) == 0)
-                delta_required(5,n_combination) = Delta;   % value of Delta that let all 8 energies match
+                delta_required(5,n_combination) = floor(log10(Delta));   % value of Delta that let all 8 energies match
               end
 
               if isequal(size(sorted_L,2),size(sorted_R,2),8) && (delta_required(6,n_combination) == 0)
-                  delta_required(6,n_combination) = Delta;  % value of Delta that let all 8 states match
+                  delta_required(6,n_combination) = floor(log10(Delta));  % value of Delta that let all 8 states match
               end
           end
         end
       if ne(delta_required(5,n_combination),0) && (delta_required(7,n_combination) == 0) && (numel(unique(ind_evals_R)) == 0)
-        delta_required(7,n_combination) = Delta;  % value of Delta that is too large to keep energies matching
+        delta_required(7,n_combination) = floor(log10(Delta));  % value of Delta that is too large to keep energies matching
       end
     end
   end
-  end       % use endwhile in Octave
+  if (Delta/(floor(log10(Delta))) == 5) | (Delta/(floor(log10(Delta))) == 10)    % update the output file and the checkpoint
+  dlmwrite(FileName,delta_required','delimiter','\t','newline','unix');   % the delta required
+  dlmwrite(FileName,Delta,'-append');     % largest Delta tested
+  checkpoint = Delta;
+  end
+  endwhile       % use end in MATLAB
   n_combination = n_combination + 1;
   end
   end
   end
-  delta_required(delta_required == 0) = nan;
-  UpdateOutput(tol,combinations,name_of_quadratization);
+  delta_required(delta_required == 0) = 308;
+  dlmwrite(FileName,delta_required','delimiter','\t','newline','unix');
+  dlmwrite(FileName,minDelta,maxDelta,'-append','delimiter','\t');     % range of Delta tested
 
 elseif length(input_choice) == 3        % test a single term
-  delta_required = zeros(7,27); combinations = cell(1,27);
-  operators = input_choice; n_combination = 1;   % to match the description in 'all_cubics' case
+  delta_required = zeros(7,1); operators = input_choice; n_combination = 1;   % to match the description in 'all_cubics' case
 
   if coefficient == -1
-  combinations = {['-' operators]};
+  FileName = strcat('negative','_',name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(test_times),'.txt');
+  input_choice = strcat('-',input_choice);
   else
-  combinations = {operators};
+  FileName = strcat('positive','_',name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(test_times),'.txt');
   end
 
   Delta = minDelta;
+  checkpoint = Delta;
   while Delta <= maxDelta
-  Delta = Delta + 10^(floor(log10(Delta))-4);
+  Delta = Delta + 10^(floor(log10(Delta))-decimal_place);
   [LHS,RHS] = lhs2rhs(coefficient,operators,Delta,name_of_quadratization);
   if isnan(RHS) == 0
     m = log2(size(RHS,2)) - length(operators);             % the number of auxiliary qubits
@@ -112,67 +125,51 @@ elseif length(input_choice) == 3        % test a single term
       sorted_R = R(:,sort(index));
       if (sum( abs(E_LHS(1:2^m) - E_RHS(1)) < tol ) == 2^m)
           if (delta_required(1,n_combination) == 0)
-              delta_required(1,n_combination) = Delta;  % value of Delta that let ground energy match
+              delta_required(1,n_combination) = floor(log10(Delta));  % value of Delta that let ground energy match
           end
           if (isempty(ind_evecs) == 0) && (delta_required(2,n_combination) == 0)
-              delta_required(2,n_combination) = Delta;  % value of Delta that let ground state match
+              delta_required(2,n_combination) = floor(log10(Delta));  % value of Delta that let ground state match
           end
 
           if (sum( abs(E_LHS((2^m + 1):2^(m+1)) - E_RHS(2)) < tol ) == 2^m)
               if (delta_required(3,n_combination) == 0)
-                  delta_required(3,n_combination) = Delta;  % value of Delta that let first excited energy match
+                  delta_required(3,n_combination) = floor(log10(Delta));  % value of Delta that let first excited energy match
               end
 
               if (size(sorted_L,2) >= 2) && (delta_required(4,n_combination) == 0)
-                  delta_required(4,n_combination) = Delta;   % value of Delta that let first excited state match
+                  delta_required(4,n_combination) = floor(log10(Delta));   % value of Delta that let first excited state match
               end
           end
 
           if (numel(unique(ind_evals_R)) == 8)
               if (delta_required(5,n_combination) == 0)
-                delta_required(5,n_combination) = Delta;   % value of Delta that let all 8 energies match
+                delta_required(5,n_combination) = floor(log10(Delta));   % value of Delta that let all 8 energies match
               end
 
               if isequal(size(sorted_L,2),size(sorted_R,2),8) && (delta_required(6,n_combination) == 0)
-                  delta_required(6,n_combination) = Delta;  % value of Delta that let all 8 states match
+                  delta_required(6,n_combination) = floor(log10(Delta));  % value of Delta that let all 8 states match
               end
           end
         end
       if ne(delta_required(5,n_combination),0) && (delta_required(7,n_combination) == 0) && (numel(unique(ind_evals_R)) == 0)
-        delta_required(7,n_combination) = Delta;  % value of Delta that is too large to keep energies matching
+        delta_required(7,n_combination) = floor(log10(Delta));  % value of Delta that is too large to keep energies matching
       end
     end
   end
-  end       %   use endwhile in Octave
-  delta_required(delta_required == 0) = nan;
-  UpdateOutput(tol,combinations,name_of_quadratization);
+  if (Delta/(floor(log10(Delta))) == 5) | (Delta/(floor(log10(Delta))) == 10)    % update the output file and the checkpoint
+  dlmwrite(FileName,delta_required','delimiter','\t','newline','unix');   % the delta required
+  dlmwrite(FileName,Delta,'-append');     % largest Delta tested
+  save -append FileName input_choice;
+  checkpoint = Delta;
+  end
+  endwhile       %   use end in MATLAB
+  delta_required(delta_required == 0) = 308;
+  dlmwrite(FileName,delta_required','delimiter','\t','newline','unix');
+  dlmwrite(FileName,minDelta,maxDelta,'-append','delimiter','\t');     % range of Delta tested
+  save -append FileName input_choice;
 
 else
   disp("Invalid Input");
 end
-
-end
-
-
-function UpdateOutput(tol,combinations,name_of_quadratization)      % Update the output text file
-
-if exist('Required_Delta.txt','file') == 0    % if the file doesn't exist, we will create a new one
-RowNames_1 = strcat(name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(1));      % gives the name of row as 'P(3->2)DC2_1e-3_1'
-RowNames_2 = strcat(name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(2));
-T = table([{'combinations'};{RowNames_1};{RowNames_2}],[combinations;num2cell(delta_required(1,:));num2cell(delta_required(2,:))],cell(1,size(T,2)));
-T = table2array(T);
-
-else      % need to be further improved so that we can get it updated during executing
-InputT = readtable('Required_Delta.txt','ReadVariableNames',false);
-InputT = table2array(InputT)';
-RowNames_1 = strcat(name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(1));      % gives the name of row as 'P(3->2)DC2_1e-3_1'
-RowNames_2 = strcat(name_of_quadratization,'_',num2str(tol,'%1.0e'),'_',num2str(2));
-T = table([{'combinations'};{RowNames_1};{RowNames_2}],[combinations;num2cell(delta_required(1,:));num2cell(delta_required(2,:))]);
-T = table2array(T);
-T = [InputT;T;cell(1,size(T,2))];
-
-end
-T = array2table(T');
-writetable(T,'Required_Delta.txt','Delimiter','\t','WriteVariableNames',false);
 
 end
