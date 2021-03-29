4b6dd43d25bc8387b27913c8050425b1fa950a91
Tim Li <77031993+tl355@users.noreply.github.com>
2021-03-29 10:04:39 -0600

    These things:
    - Added NP-OY example
    - Updated PD-BFBD example to be less resource-intensive

--- everything_else/reproducing.m
+++ everything_else/reproducing.m
@@ -293,6 +293,76 @@ end
 
 min(V_diff); % 1.3345
 
+%% NP-OY: Example (8->4-body, 4->2-body is a rewrite)
+x = [0 1 ; 1 0]; y = [0 -1i ; 1i 0]; z = [1 0 ; 0 -1];
+
+x_113  = sparse(kron(kron(speye(2^0),x),speye(2^17)));
+z_113  = sparse(kron(kron(speye(2^0),z),speye(2^17)));
+
+x_122  = sparse(kron(kron(speye(2^1),x),speye(2^16)));
+z_122  = sparse(kron(kron(speye(2^1),z),speye(2^16)));
+
+x_114  = sparse(kron(kron(speye(2^2),x),speye(2^15)));
+z_114  = sparse(kron(kron(speye(2^2),z),speye(2^15)));
+
+x_124  = sparse(kron(kron(speye(2^3),x),speye(2^14)));
+
+x_211  = sparse(kron(kron(speye(2^4),x),speye(2^13)));
+z_211  = sparse(kron(kron(speye(2^4),z),speye(2^13)));
+
+x_221  = sparse(kron(kron(speye(2^5),x),speye(2^12)));
+
+x_213  = sparse(kron(kron(speye(2^6),x),speye(2^11)));
+
+x_222  = sparse(kron(kron(speye(2^7),x),speye(2^10)));
+
+z_111  = sparse(kron(kron(speye(2^8),z),speye(2^9)));
+
+z_112  = sparse(kron(kron(speye(2^9),z),speye(2^8)));
+
+z_014  = sparse(kron(kron(speye(2^10),z),speye(2^7)));
+
+z_103  = sparse(kron(kron(speye(2^11),z),speye(2^6)));
+
+x_a111 = sparse(kron(kron(speye(2^12),x),speye(2^5)));
+y_a111 = sparse(kron(kron(speye(2^12),y),speye(2^5)));
+z_a111 = sparse(kron(kron(speye(2^12),z),speye(2^5)));
+
+x_a112 = sparse(kron(kron(speye(2^13),x),speye(2^4)));
+y_a112 = sparse(kron(kron(speye(2^13),y),speye(2^4)));
+z_a112 = sparse(kron(kron(speye(2^13),z),speye(2^4)));
+
+x_a211 = sparse(kron(kron(speye(2^14),x),speye(2^3)));
+y_a211 = sparse(kron(kron(speye(2^14),y),speye(2^3)));
+z_a211 = sparse(kron(kron(speye(2^14),z),speye(2^3)));
+
+x_a212 = sparse(kron(kron(speye(2^15),x),speye(2^2)));
+y_a212 = sparse(kron(kron(speye(2^15),y),speye(2^2)));
+z_a212 = sparse(kron(kron(speye(2^15),z),speye(2^2)));
+
+x_a121 = sparse(kron(kron(speye(2^16),x),speye(2^1)));
+y_a121 = sparse(kron(kron(speye(2^16),y),speye(2^1)));
+z_a121 = sparse(kron(kron(speye(2^16),z),speye(2^1)));
+
+x_a122 = sparse(kron(kron(speye(2^17),x),speye(2^0)));
+y_a122 = sparse(kron(kron(speye(2^17),y),speye(2^0)));
+z_a122 = sparse(kron(kron(speye(2^17),z),speye(2^0)));
+
+I = speye(2^18);
+
+U = 0.005; t = 0.375*U; J = 0.09*U;
+
+LHS = sparse(-J*(x_113*x_122*x_114*x_124*x_211*x_221*x_213*x_222 + z_111*z_112*z_113*z_114 + z_014*z_111 + z_112*z_103 + z_114*z_211 + z_113*z_122));
+
+RHS = sparse(J*(-z_111*z_112*z_113*z_114 - z_014*z_111 - z_112*z_103 - z_114*z_211 - z_113*z_122 ...
+    + (I - z_a111 + z_a112 + z_a111*z_a112) * (z_a121 + z_a122 + z_a121*z_a122 - I) ...
+    + (I + z_a111 - z_a112 + z_a111*z_a112) * (I - z_a211 - z_a212 - z_a211*z_a212)) ...
+    - (U/2)*(z_a111 + z_a112 + z_a111*z_a112 - I) ...
+    - (t/2)*((x_a112 + z_a111*x_a112)*x_113*x_114 + (x_a111*x_a112 + y_a111*y_a112)*x_122*x_124 ...
+    + (x_a112 - z_a111*x_a112)*x_221*x_222 + (x_a111*x_a112 - y_a111*y_a112)*x_211*x_213 ));
+
+abs(eigs(LHS, 1, 'smallestreal')-eigs(RHS, 1, 'smallestreal'))
+
 %% NP - SJ (4z5 - 3x1 + 2*z1*y2*x5 + 9*x1*x2*x3*x4 - x1*y2*z3*x5 -> 9*xa1 + 4*za2*z5 - 3*za3*x1 - za3*xa2 + 2*xa3*x5)
 x = [0 1;1 0];
 y = [0 -1i;1i 0];
@@ -929,39 +999,39 @@ min(V_diff); % 1.3646
 %% PD-BFBD: Example
 x = [0 1 ; 1 0]; y = [0 -1i ; 1i 0]; z = [1 0 ; 0 -1];
 
-z_4i_1_j  = sparse(kron(kron(eye(2^0),z),eye(2^13)));
-z_4i_2_j  = sparse(kron(kron(eye(2^1),z),eye(2^12)));
-z_4i_3_j  = sparse(kron(kron(eye(2^2),z),eye(2^11)));
-z_4i_4_j  = sparse(kron(kron(eye(2^3),z),eye(2^10)));
+z_4i_1_j  = sparse(kron(kron(speye(2^0),z),speye(2^13)));
+z_4i_2_j  = sparse(kron(kron(speye(2^1),z),speye(2^12)));
+z_4i_3_j  = sparse(kron(kron(speye(2^2),z),speye(2^11)));
+z_4i_4_j  = sparse(kron(kron(speye(2^3),z),speye(2^10)));
 
-x_4i_3_j  = sparse(kron(kron(eye(2^2),x),eye(2^11)));
-x_4i_4_j  = sparse(kron(kron(eye(2^3),x),eye(2^10)));
-x_4i_6_j  = sparse(kron(kron(eye(2^4),x),eye(2^9)));
-x_4i_4_j1 = sparse(kron(kron(eye(2^5),x),eye(2^8)));
+x_4i_3_j  = sparse(kron(kron(speye(2^2),x),speye(2^11)));
+x_4i_4_j  = sparse(kron(kron(speye(2^3),x),speye(2^10)));
+x_4i_6_j  = sparse(kron(kron(speye(2^4),x),speye(2^9)));
+x_4i_4_j1 = sparse(kron(kron(speye(2^5),x),speye(2^8)));
 
-x_8i_4_j  = sparse(kron(kron(eye(2^6),x),eye(2^7)));
-z_8i_4_j  = sparse(kron(kron(eye(2^6),z),eye(2^7)));
+x_8i_4_j  = sparse(kron(kron(speye(2^6),x),speye(2^7)));
+z_8i_4_j  = sparse(kron(kron(speye(2^6),z),speye(2^7)));
 
-x_8i_6_j  = sparse(kron(kron(eye(2^7),x),eye(2^6)));
-z_8i_6_j  = sparse(kron(kron(eye(2^7),z),eye(2^6)));
+x_8i_6_j  = sparse(kron(kron(speye(2^7),x),speye(2^6)));
+z_8i_6_j  = sparse(kron(kron(speye(2^7),z),speye(2^6)));
 
-x_8i_3_j1 = sparse(kron(kron(eye(2^8),x),eye(2^5)));
-z_8i_3_j1 = sparse(kron(kron(eye(2^8),z),eye(2^5)));
+x_8i_3_j1 = sparse(kron(kron(speye(2^8),x),speye(2^5)));
+z_8i_3_j1 = sparse(kron(kron(speye(2^8),z),speye(2^5)));
 
-x_8i_5_j1 = sparse(kron(kron(eye(2^9),x),eye(2^4)));
-z_8i_5_j1 = sparse(kron(kron(eye(2^9),z),eye(2^4)));
+x_8i_5_j1 = sparse(kron(kron(speye(2^9),x),speye(2^4)));
+z_8i_5_j1 = sparse(kron(kron(speye(2^9),z),speye(2^4)));
 
-x_8i_1_j  = sparse(kron(kron(eye(2^10),x),eye(2^3)));
-z_8i_1_j  = sparse(kron(kron(eye(2^10),z),eye(2^3)));
+x_8i_1_j  = sparse(kron(kron(speye(2^10),x),speye(2^3)));
+z_8i_1_j  = sparse(kron(kron(speye(2^10),z),speye(2^3)));
 
-x_8i_2_j  = sparse(kron(kron(eye(2^11),x),eye(2^2)));
-z_8i_2_j  = sparse(kron(kron(eye(2^11),z),eye(2^2)));
+x_8i_2_j  = sparse(kron(kron(speye(2^11),x),speye(2^2)));
+z_8i_2_j  = sparse(kron(kron(speye(2^11),z),speye(2^2)));
 
-x_8i_7_j  = sparse(kron(kron(eye(2^12),x),eye(2^1)));
-z_8i_7_j  = sparse(kron(kron(eye(2^12),z),eye(2^1)));
+x_8i_7_j  = sparse(kron(kron(speye(2^12),x),speye(2^1)));
+z_8i_7_j  = sparse(kron(kron(speye(2^12),z),speye(2^1)));
 
-x_8i_8_j  = sparse(kron(kron(eye(2^13),x),eye(2^0)));
-z_8i_8_j  = sparse(kron(kron(eye(2^13),z),eye(2^0)));
+x_8i_8_j  = sparse(kron(kron(speye(2^13),x),speye(2^0)));
+z_8i_8_j  = sparse(kron(kron(speye(2^13),z),speye(2^0)));
 
 
 LHS = sparse(-(z_4i_1_j*z_4i_2_j*z_4i_3_j*z_4i_4_j + x_4i_3_j*x_4i_4_j*x_4i_6_j*x_4i_4_j1));
